65a857fbfc4e7ec7fa58e9bd32f30af272ccd70d
umersaid <97168781+umersaid@users.noreply.github.com>
2024-01-04 21:35:20 +0500

    added method fileWriter

--- ReaderAndWriter.java
+++ ReaderAndWriter.java
@@ -3,5 +3,15 @@ import java.io.FileWriter;
 import java.util.ArrayList;
 
 public class ReaderAndWriter {
-    
+    public static void fileWriter(Task task) {
+        try {
+            String taskStr = task.getDate() + "," + task.getTime() + "," + task.getTaskDesc() + "," + task.getStatus()
+                    + "\n";
+            FileWriter writer = new FileWriter("Tasks-List.csv", true);
+            writer.write(taskStr);
+            writer.close();
+        } catch (Exception e) {
+            System.out.print(e);
+        }
+    }
 }
